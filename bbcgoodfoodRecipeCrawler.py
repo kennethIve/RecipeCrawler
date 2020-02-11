aa4447ffbee444b9e097ef6d5277749e7ef9f467
Kenneth Huang Jinpei <jpkhuang@shkp.com>
2020-02-11 16:49:19 +0800

    update Commit

--- bbcgoodfoodRecipeCrawler.py
+++ bbcgoodfoodRecipeCrawler.py
@@ -5,7 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 #library
-import requests
+import requests ,json
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
@@ -22,20 +22,25 @@ def getSteps(pageSoup):
         stepList.append(steps.text)
     return stepList
 
-class recipeObject:
+class RecipeObject(object):
     def __init__(self,recipeName,ingredients,steps):
         self.recipeName = recipeName
         self.ingredients = ingredients
         self.steps = steps;
         
 #main class to run
+#url and domain for website
 url = 'https://www.bbcgoodfood.com/recipes/collection/easy'
 domain = 'https://www.bbcgoodfood.com'
+#location for phantomJs driver
+phanJsLocation = 'C:\\Users\\jpkhuang\\Desktop\\installer\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs'
+#location to store the json
+location = "C:\\Users\\jpkhuang\\Desktop\\result.json"
 counter = 1
 recipeList = []
 #req = requests.get(url)
 #print(req.text)
-driver = webdriver.PhantomJS(executable_path='C:\\Users\\jpkhuang\\Desktop\\installer\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs')
+driver = webdriver.PhantomJS(executable_path=phanJsLocation)
 driver.get(url)
 # get the home page of the page of the recipe page
 print(driver.current_url)
@@ -46,22 +51,22 @@ for recipe in soup.select('{}'.format('article .node-recipe')):
     #get the dish name
     recipeName = recipe.select('.teaser-item__title')[0].text
     print('---------'+str(counter)+' '+recipeName+' Start--------------')
-    print();
     #get the url link to redirect to the recipe page
     recipeUrl = domain+recipe.select('.teaser-item__image > a')[0]['href']    
-    print('Loading url: %s',recipeUrl)
+    print('Loading url:',recipeUrl)
     driver.get(recipeUrl)
     pageSoup = BeautifulSoup(driver.page_source,'lxml')    
     #pushrecipe
     print('push recipe into list...')
-    recipeList.append(recipeObject(recipeName,getIngredients(pageSoup),getSteps(pageSoup)))
+    recipeList.append(RecipeObject(recipeName,getIngredients(pageSoup),getSteps(pageSoup)))
     print('---------'+str(counter)+' '+recipeName+' end ---------')
-    counter+=1
-
+    counter+=1    
 
 driver.close()
-print(recipeList[24])
 print('\n---------Recipe crawler end---------')
+#write into json file
+with open(location, "w") as file:
+    file.write(json.dumps([recipe.__dict__ for recipe in recipeList]))
 
 
         
