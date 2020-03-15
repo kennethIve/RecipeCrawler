fe3bd65e489e097a6bdb5e2f06244645647ddd20
kenneth <kenneth96@outlook.com>
2020-03-15 17:51:48 +0800

    runnable sample of tensorflow

--- RecipeScrapy/tensorflow/tf.py
+++ RecipeScrapy/tensorflow/tf.py
@@ -3,12 +3,26 @@ from keras.models import Model
 import tensorflow as tf
 import numpy as np
 
-modelPath = "C:\\Users\\jpkhuang\\Desktop\\Crawler\\RecipeScrapy\\tensorflow\\detect.tflite"
+#modelPath = "C:\\Users\\jpkhuang\\Desktop\\Crawler\\RecipeScrapy\\tensorflow\\detect.tflite"
+modelPath = "C:\\FYP\\python_code\\RecipeCrawler\\RecipeScrapy\\tensorflow\\detect.tflite"
 print("loading tflite model")
 #interpreter = tf.lite.Interpreter(model_path="model\detect.tflite")
 interpreter = tf.lite.Interpreter(model_path=modelPath)
 interpreter.allocate_tensors()
 
-print("testing model")
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+# 函数 `get_tensor()` 会返回一份张量的拷贝。
+# 使用 `tensor()` 获取指向张量的指针。
+tflite_results = interpreter.get_tensor(output_details[0]['index'])
+
+print(input_data)
+# 对比结果。
+for  tflite_result in tflite_results:
+  print(tflite_result) 
